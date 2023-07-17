3942833385f430674af445886b55b05af97dba48
bob <dvq10071996@gmail.com>
2023-07-17 14:07:55 +0700

    update code

--- lib/src/constances/assets_path.dart
+++ lib/src/constances/assets_path.dart
@@ -2,4 +2,5 @@ class ImageAssetPath {
   ImageAssetPath._();
   static const imageGoogle = 'assets/images/google.png';
   static const imageApple = 'assets/images/apple.png';
+  static const a = '';
 }
