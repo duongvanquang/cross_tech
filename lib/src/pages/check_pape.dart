a8a52b03acafa30c94b4a576634daf9d3c9f3686
bob <dvq10071996@gmail.com>
2023-07-23 15:30:27 +0700

    update code

---
+++ lib/src/pages/check_pape.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Checkpage extends StatefulWidget {
+  const Checkpage({super.key});
+
+  @override
+  State<Checkpage> createState() => _CheckpageState();
+}
+
+class _CheckpageState extends State<Checkpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.yellowAccent,
+    );
+  }
+}
